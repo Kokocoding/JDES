3e328076858541bc2f2b7ad70011844d9b78ccf6
stytle704726 <stytle704726@gmail.com>
2023-08-10 14:07:21 +0800

    no message

--- app/src/main/java/com/example/jdes/MainActivity.kt
+++ app/src/main/java/com/example/jdes/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.hoho.android.usbserial.driver.UsbSerialPort
 import com.hoho.android.usbserial.driver.UsbSerialProber
 import kotlinx.coroutines.*
@@ -13,7 +14,7 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    private var timingJob: Job? = null // 用于存储定时任务的 Job 对象
+    private var timingJob: Job? = null // 用於存儲定時任務的 Job 對象
     private lateinit var port: UsbSerialPort
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
@@ -36,28 +37,27 @@ class MainActivity : AppCompatActivity() {
         openPort()
 
         coroutineScope.launch {
-            while (isActive) { // 在协程活动期间持续监听
-                val data = readData() // 调用您的 readData() 函数来读取数据
+            while (isActive) { // 在活動期間持續監聽
+                val data = readData()
                 processData(data)
-                delay(300) // 每秒监听一次，根据实际需求调整延迟时间
+                delay(200) // 每0.2秒一次
             }
         }
     }
 
     private suspend fun processData(data: ByteArray) {
-        // 在这里处理接收到的数据
-        // 根据实际需求解析和处理 data 数组
+        // 在這里處理接收到的數據
         val temper = mapOf("11" to R.id.Temperature1, "12" to R.id.Temperature2, "13" to R.id.Temperature3, "14" to R.id.Temperature4,
-                           "15" to R.id.Temperature5, "16" to R.id.Temperature6, "17" to R.id.Temperature7, "18" to R.id.Temperature8)
+            "15" to R.id.Temperature5, "16" to R.id.Temperature6, "17" to R.id.Temperature7, "18" to R.id.Temperature8)
         val temperInWater = mapOf("19" to R.id.TemperatureWI1, "1a" to R.id.TemperatureWI2)
         val temperOutWater = mapOf("19" to R.id.TemperatureWO1, "1a" to R.id.TemperatureWO2)
 
         if (data.isNotEmpty() && data[0] == 0x7A.toByte()) {
-            when (val dataType = data[1].toUByte().toString(16)) { // 将第二个字节转换为十六进制字符串
+            when (val dataType = data[1].toUByte().toString(16)) {
                 in temper.keys -> {
                     val textViewId = temper[dataType]
                     val textView = findViewById<TextView>(textViewId!!)
-                    val temperature = data[2].toInt() // 假设温度数据在第三个字节
+                    val temperature = data[2].toInt()
                     val temperatureStr = "$temperature°C"
 
                     withContext(Dispatchers.Main) {
@@ -70,8 +70,8 @@ class MainActivity : AppCompatActivity() {
                     val textViewIdOut = temperOutWater[dataType]
                     val textViewOut = findViewById<TextView>(textViewIdOut!!)
 
-                    val temperatureIn = data[2].toInt() // 假设温度数据在第三个字节
-                    val temperatureOut = data[3].toInt() // 假设温度数据在第三个字节
+                    val temperatureIn = data[2].toInt()
+                    val temperatureOut = data[3].toInt()
 
                     val temperatureStrI = "$temperatureIn°C"
                     val temperatureStrO = "$temperatureOut°C"
@@ -86,22 +86,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
+    //switch UI
     fun switchChange(view: View){
         val tag = view.tag
 
+        // change send Data
+        @SuppressLint("SetTextI18n")
         fun setImageResourceAndCmd(imageViewId: Int, isSwitchChecked: Boolean, index: Int) {
             val cmd: ByteArray
             val switch = findViewById<Switch>(switchIds[index])
             val imageView = findViewById<ImageView>(imageViewId)
 
             switch.isChecked = isSwitchChecked
-            imageView.setImageResource(if(isSwitchChecked) R.drawable.green else R.drawable.red)
+            imageView.setImageResource(if (isSwitchChecked) R.drawable.green else R.drawable.red)
 
             val indexByte = (index + 1).toByte()
             val dataByte = if (isSwitchChecked) 0x0A.toByte() else 0x0B.toByte()
             val fourthByte = (indexByte + dataByte).toByte()
             cmd = byteArrayOf(0x7A, indexByte, dataByte, fourthByte, 0xBB.toByte())
-            writeData(cmd)
+
+//            val tv = findViewById<TextView>(R.id.textView4)
+//            val tvText = tv.text
+//            val byteArrayString = cmd.joinToString(" ") { it.toString(16).padStart(2, '0') }
+//            tv.text = "$tvText\n$byteArrayString"
+
+            lifecycleScope.launch {
+                writeData(cmd)
+                delay(500) // 添加延遲
+            }
         }
 
         if (tag is String) {
@@ -113,33 +125,46 @@ class MainActivity : AppCompatActivity() {
             val switchG2 = findViewById<Switch>(R.id.switchGroup2)
             val switchG3 = findViewById<Switch>(R.id.switchGroup3)
 
-            when(tagInt) {
+            when (tagInt) {
                 124, 125, 126, 127 -> {
                     for ((index, imageViewId) in imagesIds.withIndex()) {
-                        if(tagInt == 124 && index < 4) setImageResourceAndCmd(imageViewId, isSwitchChecked, index)
-                        if(tagInt == 125 && (index == 4 || index == 5)) setImageResourceAndCmd(imageViewId, isSwitchChecked, index)
-                        if(tagInt == 126 && (index == 6 || index == 7)) setImageResourceAndCmd(imageViewId, isSwitchChecked, index)
-                        if(tagInt == 127){
+                        if (tagInt == 124 && index < 4) setImageResourceAndCmd(
+                            imageViewId,
+                            isSwitchChecked,
+                            index
+                        )
+                        if (tagInt == 125 && (index == 4 || index == 5)) setImageResourceAndCmd(
+                            imageViewId,
+                            isSwitchChecked,
+                            index
+                        )
+                        if (tagInt == 126 && (index == 6 || index == 7)) setImageResourceAndCmd(
+                            imageViewId,
+                            isSwitchChecked,
+                            index
+                        )
+                        if (tagInt == 127) {
                             setImageResourceAndCmd(imageViewId, isSwitchChecked, index)
                             switchG1.isChecked = switchAllCall.isChecked
                             switchG2.isChecked = switchAllCall.isChecked
                             switchG3.isChecked = switchAllCall.isChecked
-                        }else{
+                        } else {
                             switchAllCall.isChecked = switchG1.isChecked && switchG2.isChecked && switchG3.isChecked
                         }
                     }
                 }
-                else -> {
-                    setImageResourceAndCmd(imagesIds[tagInt-1], isSwitchChecked, tagInt-1)
 
-                    switchG1.isChecked = findViewById<Switch>(switchIds[0]).isChecked && findViewById<Switch>(switchIds[1]).isChecked &&
-                            findViewById<Switch>(switchIds[2]).isChecked && findViewById<Switch>(switchIds[3]).isChecked
+                else -> {
+                    setImageResourceAndCmd(imagesIds[tagInt - 1], isSwitchChecked, tagInt - 1)
+                    switchG1.isChecked = findViewById<Switch>(switchIds[0]).isChecked && findViewById<Switch>(switchIds[1]).isChecked
+                                      && findViewById<Switch>(switchIds[2]).isChecked && findViewById<Switch>(switchIds[3]).isChecked
                     switchG2.isChecked = findViewById<Switch>(switchIds[4]).isChecked && findViewById<Switch>(switchIds[5]).isChecked
                     switchG3.isChecked = findViewById<Switch>(switchIds[6]).isChecked && findViewById<Switch>(switchIds[7]).isChecked
                     switchAllCall.isChecked = switchG1.isChecked && switchG2.isChecked && switchG3.isChecked
                 }
             }
         }
+
     }
 
     private fun openPort(){
@@ -166,7 +191,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun readData(): ByteArray {
         //read
-        val bufferSize = 1024 // 适当的缓冲区大小，根据实际需求调整
+        val bufferSize = 1024
         val buffer = ByteArray(bufferSize)
         var bytesRead = 0
 
@@ -176,7 +201,7 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
 
-        // 根据实际读取的字节数截取数据
+        // 根據實際讀取的字節數截取數據
         return buffer.copyOf(bytesRead)
     }
 
@@ -198,7 +223,7 @@ class MainActivity : AppCompatActivity() {
             view.visibility = View.GONE
             closeButton.visibility = View.VISIBLE
 
-            var remainingTime = timeInMillis / 1000 // 将毫秒转换为秒
+            var remainingTime = timeInMillis / 1000 // 將毫秒轉換為秒
             while (remainingTime > 0) {
                 val hour = remainingTime/60/60
                 val min = remainingTime/60%60
